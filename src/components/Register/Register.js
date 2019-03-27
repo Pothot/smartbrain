8674da6c79a32148a34858bb1599ea2b33d46242
Hotpot <attopf@gmail.com>
2019-03-27 18:07:31 +0100

    commit

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3002/register', {
+    fetch('https://tranquil-dawn-18951.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
